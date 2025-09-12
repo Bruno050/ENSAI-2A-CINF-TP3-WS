bd199d4c677581f12dcf70a9a8a55635cd08dd06
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-09-12 14:07:16 +0000

    j'ai completé les classes du dossiers attack sans les implementer toute pour avancer dans le TP3

---
+++ src/business_object/attack/physical_formula_attack.py
@@ -0,0 +1,17 @@
+from busniess_object.attack.abstract_formula_attack import AbstractFormulaAttack
+
+from business_object.pokemon.abstract_pokemon import AbstractPokemon
+
+
+class PhysicalFormulaAttack(AbstractFormulaAttack):
+    def __init__(self, power=0, name=None, description=None):
+        super().__init__(power=power, name=name, description=description)
+
+    def compute_damage(pk1: AbstractPokemon, pk2: AbstractPokemon) -> int:
+        pass
+
+    def get_attack_stat(pk1: AbstractPokemon) -> float:
+        pass
+
+    def get_defense_stat(pk1: AbstractPokemon) -> float:
+        pass
